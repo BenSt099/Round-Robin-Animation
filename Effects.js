5904414478b47211932710a1ce47d3c8fb4a0205
BenSt099 <ben.steinhauer.de@gmail.com>
2022-12-30 15:28:16 +0100

    update whole project; added license

---
+++ Effects.js
@@ -0,0 +1,29 @@
+function hideAllSquares(){
+    document.getElementById('sq2').style.visibility = "hidden";
+    document.getElementById('sq3').style.visibility = "hidden";
+    document.getElementById('sq4').style.visibility = "hidden";
+}
+function getYellow() {
+
+    document.getElementById('sq1').style.transition = "4s";
+    document.getElementById('sq1').style.transform = "translateX(-40px)";
+    
+    document.getElementById('sq2').style.transform = "translateX(-40px)";
+    document.getElementById('sq2').style.transition = "4s";
+    document.getElementById('sq2').style.visibility = "visible";
+    document.getElementById('sq2').style.transform = "translateY(-120px)";
+    
+    document.getElementById('sq3').style.transform = "translateX(-40px)";
+    document.getElementById('sq3').style.transform = "translateY(-120px)";
+    document.getElementById('sq3').style.transition = "4s";
+    document.getElementById('sq3').style.visibility = "visible";
+    document.getElementById('sq3').style.transform = "translateX(80px)";
+    
+    document.getElementById('sq4').style.transform = "translateX(-40px)";
+    document.getElementById('sq4').style.transform = "translateY(-120px)";
+    document.getElementById('sq4').style.transform = "translateX(80px)";
+    document.getElementById('sq4').style.transition = "4s";
+    document.getElementById('sq4').style.visibility = "visible";
+    document.getElementById('sq4').style.transform = "translateX(-160px)";
+    
+}
