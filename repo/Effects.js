fdfed294dbbfa63d6ec2845186ed7c913da2d085
BenSt099 <ben.steinhauer.de@gmail.com>
2022-12-30 16:52:38 +0100

    changed animation

--- repo/Effects.js
+++ repo/Effects.js
@@ -6,28 +6,18 @@ function hideAllSquares(){
 
 function spreadOut() {
 
-    document.getElementById('sq1').style.transition = "4s";
-    document.getElementById('sq1').style.transform = "translateX(-40px)";
-    
     document.getElementById('sq2').style.transform = "translateX(-40px)";
+    document.getElementById('sq2').style.transform = "translateY(-120px)";
+    document.getElementById('sq2').style.transform = "translateX(80px)";
     document.getElementById('sq2').style.transition = "4s";
     document.getElementById('sq2').style.visibility = "visible";
-    document.getElementById('sq2').style.transform = "translateY(-120px)";
-    
-    document.getElementById('sq3').style.transform = "translateX(-40px)";
-    document.getElementById('sq3').style.transform = "translateY(-120px)";
+    document.getElementById('sq2').style.transform = "translateX(-119px)";
+
     document.getElementById('sq3').style.transition = "4s";
     document.getElementById('sq3').style.visibility = "visible";
-    document.getElementById('sq3').style.transform = "translateX(80px)";
-    
-    document.getElementById('sq4').style.transform = "translateX(-40px)";
-    document.getElementById('sq4').style.transform = "translateY(-120px)";
-    document.getElementById('sq4').style.transform = "translateX(80px)";
+    document.getElementById('sq3').style.transform = "translate(-120px, -120px)";
+
     document.getElementById('sq4').style.transition = "4s";
     document.getElementById('sq4').style.visibility = "visible";
-    document.getElementById('sq4').style.transform = "translateX(-160px)";
-
-    setTimeout(() => {document.getElementById('sq2').style.visibility = "hidden";
-                      document.getElementById('sq3').style.visibility = "hidden";
-                      document.getElementById('sq4').style.visibility = "hidden";}, 3000);
+    document.getElementById('sq4').style.transform = "translateY(-120px)";
 }
